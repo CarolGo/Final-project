987d3e32e7318ca197c4a48aefff606abc24c7c6
CarolGo <yuyaow3@illinois.edu>
2017-12-13 14:45:16 -0600

    delete mysite

--- mysite/user/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import user, invest, user_invest, question, myindex
-admin.site.register(user)
-admin.site.register(invest)
-admin.site.register(user_invest)
-admin.site.register(question)
--- mysite/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-app_name = 'user'
-urlpatterns = [
-    url(r'^form/$', views.form, name= 'form'),
-    url(r'^test/$', views.test, name= 'test'),
-    url(r'^buy/$', views.buy, name='buy'),
-    url(r'^plo/$', views.plo, name= 'plo'),
-]
