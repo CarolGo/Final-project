960568d58c5e1a54a6669f4896f0912b13c9b4df
Haoyuan Song <haoyuan6@illinois.edu>
2019-01-11 19:21:11 -0600

    Reformat the code style. Fixed several bugs Improved algorithm efficiency

--- choose.py
+++ choose.py
@@ -16,23 +16,22 @@ def choose(all_stock, basic_start, basic_end, risk_a, processor, basic_times, st
     '''
     val_set = []
     # get all testing data.
-    port = port_fin.Portfolio(all_stock)
-    port.set_basic(basic_start, basic_end, risk_a, processor)
-    returns = port.get_data(port)
+    port = port_fin.Portfolio(all_stock, basic_start, basic_end, risk_a, processor)
+    returns = port.get_data()
     # find all the portfolio which contains two stocks and append them into a list
     for i in range(len(all_stock)):
         last = all_stock.pop()
+        print(last)
         if len(all_stock) == 0:
             break
         for j in range(len(all_stock)):
             # generate the portfolio
             temp = [last, all_stock[j]]
-            temp_port = port_fin.Portfolio(temp)
-            temp_port.set_basic(basic_start, basic_end, risk_a, processor)
+            temp_port = port_fin.Portfolio(temp, basic_start, basic_end, risk_a, processor)
             # get data from all the testing data
             temp_port.returns = pd.concat(
                 [returns.iloc[:, i], returns.iloc[:, j]], join='outer', axis=1)
-            max_val = temp_port.plo(temp_port, 0, basic_times)[1]
+            max_val = temp_port.plo(0, basic_times)[1]
             # generate a list containing the portfolios and their max utility
             val_set.append([temp, max_val])
     # sort the list by their max utility
@@ -47,20 +46,19 @@ def choose(all_stock, basic_start, basic_end, risk_a, processor, basic_times, st
 
 if __name__ == '__main__':
     # setting the initial value
-    all_stock = ['MSFT', 'GE', 'C', 'AMD', 'AAPL', 'F', 'T', 'NKE', 'CTL']
-    basic_start = '2017-03-20'
-    basic_end = '2017-05-20'
+    all_stock = ['MSFT', 'GE', 'C', 'AMD', 'AAPL']
+    basic_start = '2018-03-20'
+    basic_end = '2018-04-20'
     risk_a = 5.5
     processor = 4
-    basic_times = 150
+    basic_times = 10
     stock_num = 3
     # use choose to find selected stocks
     select_stock = choose(all_stock, basic_start, basic_end,
                           risk_a, processor, basic_times, stock_num)
     print("The choosen stocks are:", select_stock)
     # generate the final portfolio
-    fin_port = port_fin.Portfolio(select_stock)
-    fin_port.set_basic('2016-01-01', '2017-05-20', risk_a, processor)
-    fin_port.get_data(fin_port)
-    max_we = fin_port.plo(fin_port, 1, 4000)[0]
-    fin_port.print(fin_port, max_we)
+    fin_port = port_fin.Portfolio(select_stock, '2016-01-01', '2017-05-20', risk_a, processor)
+    fin_port.get_data()
+    max_we = fin_port.plo(1, 4000)[0]
+    fin_port.print(max_we)
